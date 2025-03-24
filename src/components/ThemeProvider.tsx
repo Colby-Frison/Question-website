4f4cab9dd333c0adefa97d5130ce53c5551eb3c4
Colby-Frison <colbyfrison100@gmail.com>
2025-03-24 11:07:54 -0500

    Force lightmode theme

--- src/components/ThemeProvider.tsx
+++ src/components/ThemeProvider.tsx
@@ -30,7 +30,7 @@ export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return (
     <NextThemesProvider
       attribute="class"
-      defaultTheme="system"
+      defaultTheme="light"
       enableSystem
       storageKey="theme"
       disableTransitionOnChange={false}
